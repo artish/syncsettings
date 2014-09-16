b6f0b857503f22f4e93e0565c0636064ff3d3638
Florian Schroedl Home <flo.schroedl@gmail.com>
2014-09-16 14:07:36 +0200

    Formated the script to linting standards

--- setup_settings.py
+++ setup_settings.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python
-# 
+#
 # Setup Settings
 #
-#==============================================================================#
+#=============================================================================#
 # Imports
-#==============================================================================#
+#=============================================================================#
 
 # System Modules
 import os
 import sys
 import glob
 import json
-from pprint import pprint
 
 # Custom Modules
-from lib.send2trash import send2trash # https://github.com/hsoft/send2trash
+from lib.send2trash import send2trash  # https://github.com/hsoft/send2trash
 
-#==============================================================================#
+#=============================================================================#
 # Global Variables
-#==============================================================================#
+#=============================================================================#
 
 mode = ""
 # mode = "test"
@@ -26,164 +25,180 @@ mode = ""
 # Name of the settings file
 cfg_file = "sync_settings.json"
 
-#==============================================================================#
+#=============================================================================#
 # Functions
-#==============================================================================#
+#=============================================================================#
 
-# symlink(current directory, symlink source folder, symlink destination folder )
-# Creates a symlink from a file/path to a give path
+
 def symlink(cur, json, src, dst, title, overwrite_all=None):
 
-  # if (title): 
-  #   print title
+    # Grab the path from the json parent dir
+    json = os.path.dirname(json)
+    src = json + "/" + src
 
-  # Grab the path from the json parent dir
-  json = os.path.dirname(json)
-  src = json + "/" + src
-  
-  # Expand the paths to the current user
-  dst = os.path.expanduser(dst)
+    # Expand the paths to the current user
+    dst = os.path.expanduser(dst)
 
-  if not any( [os.path.isfile(src), os.path.isdir(src), os.path.islink(src)] ):
+    if not any([
+            os.path.isfile(src),
+            os.path.isdir(src),
+            os.path.islink(src)]
+            ):
 
-    print "ERROR: The source doesnt exist"
-    print title
-    print src
-    print
-    return
+        print "ERROR: The source doesnt exist"
+        print title
+        print src
+        print
+        return
 
-  # If the target is an existing file or folder
-  # Ask to overwrite it or to skip it
-  if any( [os.path.isfile(dst), os.path.isdir(dst), os.path.islink(dst)] ):
+    # If the target is an existing file or folder
+    # Ask to overwrite it or to skip it
+    if any([
+        os.path.isfile(dst),
+        os.path.isdir(dst),
+        os.path.islink(dst)]
+            ):
 
-    # TODO 001: Remove this unecessary if condition
+          # TODO 001: Remove this unecessary if condition
 
-    # When overwrite_all is "y" or in testing mode
-    # Overwrite the file
-    if any ( [overwrite_all] ):
+          # When overwrite_all is "y" or in testing mode
+          # Overwrite the file
+            if any(overwrite_all):
 
-      overwrite_all = 1
+                overwrite_all = 1
 
-    else: 
+            else:
 
-      print "File " + dst + " already exists."
-      
-      # Check the prompt value for
-      #
-      # a: Overwrite all files and don't ask anymore if files should be overwritten
-      # y: Continue the function and overwrite
-      # n: Exit the function and don't overwrite
-      #
-      # On any other input repeat the prompt
-      
-      prompt = ""
-      
-      while all ([
-                   prompt != "y", 
-                   prompt != "n", 
-                   prompt != "a"
-                  ]):
-        prompt = raw_input("Overwrite (y/n/a): ")
+                print "File " + dst + " already exists."
 
-      if (prompt == "a"):
-        overwrite_all = 1 # Overwrite All
-      elif (prompt == "n"):
-        return # Exit
-      elif (prompt == "y"):
-        pass # Overwrite only this file
+                # Check the prompt value for
+                #
+                # a: Overwrite all files and don't ask anymore
+                # y: Continue the function and overwrite
+                # n: Exit the function and don't overwrite
+                #
+                # On any other input repeat the prompt
 
-  # Trash the resulting destination file or dir
-  # And delete symlinks so they don't crowd the trashbin
-  if (os.path.islink(dst)):
-    os.unlink(dst)
-  elif any ([os.path.isfile(dst), os.path.isdir(dst)]): 
-    send2trash(dst)
+                prompt = ""
 
-  # Create the symlink
-  os.symlink(src, dst)
+                while all([
+                    prompt != "y",
+                    prompt != "n",
+                    prompt != "a"
+                        ]):
 
-  # Print the symlink creation information
-  # print "Creating SymLink: "
-  # print "  Src: " + src
-  # print "  Dst: " + dst + "\n"
+                    prompt = raw_input("Overwrite (y/n/a): ")
 
-  return overwrite_all
+                    if (prompt == "a"):
+                        overwrite_all = 1  # Overwrite All
+                    elif (prompt == "n"):
+                        return   # Exit
+                    elif (prompt == "y"):
+                        pass   # Overwrite only this file
 
-# Moves given Objects to trash or unlinks symlinks
+    # Trash the resulting destination file or dir
+    # And delete symlinks so they don't crowd the trashbin
+    if (os.path.islink(dst)):
+        os.unlink(dst)
+    elif any([os.path.isfile(dst), os.path.isdir(dst)]):
+        send2trash(dst)
+
+    # Create the symlink
+    os.symlink(src, dst)
+
+    # Print the symlink creation information
+    # print "Creating SymLink: "
+    # print "  Src: " + src
+    # print "  Dst: " + dst + "\n"
+
+    return overwrite_all
+
+
 def trash(dst):
-  
-  if any([os.path.isfile(dst), os.path.isdir(dst)]):
-    send2trash(dst)
-  elif os.path.islink(dst):
-    os.unlink(dst)
 
-#==============================================================================#
+    """Moves given Objects to trash or unlinks symlinks"""
+
+    if any([os.path.isfile(dst), os.path.isdir(dst)]):
+        send2trash(dst)
+    elif os.path.islink(dst):
+        os.unlink(dst)
+
+#=============================================================================#
 # Main
-#==============================================================================#
+#=============================================================================#
+
 
 def main(argv=None):
 
-  #----------------------------------------------------------------------------#
-  # Variables
-  #----------------------------------------------------------------------------#
+    #-------------------------------------------------------------------------#
+    # Variables
+    #-------------------------------------------------------------------------#
 
-  # If this is true all existing files will be overwriteen
-  overwrite_all = 0
+    # If this is true all existing files will be overwriteen
+    overwrite_all = 0
 
-  # Path with the settings
-  settings_dir = "~/Settings"
-  settings_dir = os.path.expanduser(settings_dir)
+    # Path with the settings
+    settings_dir = "~/Settings"
+    settings_dir = os.path.expanduser(settings_dir)
 
-  # Check if there is either a symlink or a dir ~/Settings
-  # After that check if there is a Settings dir in the parent dir of the script
-  # If none of these exist, exit the script
-  if (os.path.islink(settings_dir)):
-    settings_dir = (os.path.realpath(os.path.expanduser(settings_dir))) # Expand the user and find the symlink target path
-  elif (os.path.isdir(settings_dir)):
-    settings_dir = (os.path.expanduser(settings_dir)) # Expand the user and find the symlink target path
-  else:
-    print "No Settings folder found in either ~/ or the script dir!"
-    return
+    # Check if there is either a symlink or a dir ~/Settings
+    # After that check if there is a Settings dir in the root dir of the script
+    # If none of these exist, exit the script
+    if (os.path.islink(settings_dir)):
+        # Expand the user and find the symlink target path
+        settings_dir = (os.path.realpath(os.path.expanduser(settings_dir)))
+    elif (os.path.isdir(settings_dir)):
+        # Expand the user and find the symlink target path
+        settings_dir = (os.path.expanduser(settings_dir))
+    else:
+        print "No Settings folder found in either ~/ or the script dir!"
+        return
 
-  # Get all sync setting files
-  # Or just the test folder in test mode
-  if (mode != "test"):
-    # TODO 002: Replace this with a walk function
-    cfg  = glob.glob(settings_dir + '*/' + cfg_file)
-    cfg += glob.glob(settings_dir + '*/*/' + cfg_file)
-    cfg += glob.glob(settings_dir + '*/*/*/' + cfg_file)
-    cfg += glob.glob(settings_dir + '*/*/*/*/' + cfg_file)
-  else:
-    cfg = [settings_dir + '/test/' + cfg_file]
+      # Get all sync setting files
+      # Or just the test folder in test mode
+    if (mode != "test"):
+        # TODO 002: Replace this with a walk function
+        cfg = glob.glob(settings_dir + '*/' + cfg_file)
+        cfg += glob.glob(settings_dir + '*/*/' + cfg_file)
+        cfg += glob.glob(settings_dir + '*/*/*/' + cfg_file)
+        cfg += glob.glob(settings_dir + '*/*/*/*/' + cfg_file)
+    else:
+        cfg = [settings_dir + '/test/' + cfg_file]
 
-  # If there aren't any config files exit the script
-  if not(cfg):
-    print ("No %s files found!" % cfg_file)
-    return
+    # If there aren't any config files exit the script
+    if not(cfg):
+        print ("No %s files found!" % cfg_file)
+        return
 
-  #----------------------------------------------------------------------------#
-  # Symlink Iteration
-  #----------------------------------------------------------------------------#
+    #-------------------------------------------------------------------------#
+    # Symlink Iteration
+    #-------------------------------------------------------------------------#
 
-  # Loop through the configuration files and create the symlinks
-  for x in cfg:
+    # Loop through the configuration files and create the symlinks
+    for x in cfg:
 
-    # Load the data from the json files into data
-    json_data = open(x)
-    data = json.load(json_data)
-    json_data.close()
+        # Load the data from the json files into data
+        json_data = open(x)
+        data = json.load(json_data)
+        json_data.close()
 
-    # Get the folders to trash if they are available
-    if "trash" in data:
-      for t in data["trash"]:
-        trash(t)
+        # Get the folders to trash if they are available
+        if "trash" in data:
+            for t in data["trash"]:
+                trash(t)
 
-    if "symlink" in data:
-      for d in data["symlink"]:
-        overwrite_all = symlink(settings_dir, x, d["src"], d["dst"], d["title"], overwrite_all)
+        if "symlink" in data:
+            for d in data["symlink"]:
+
+                overwrite_all = symlink(
+                    settings_dir,
+                    x,
+                    d["src"], d["dst"],
+                    d["title"],
+                    overwrite_all)
 
 # Advanced main function call
 # http://www.artima.com/weblogs/viewpost.jsp?thread=4829
 if __name__ == "__main__":
-  sys.exit(main())
-  pass
+    sys.exit(main())
+    pass
